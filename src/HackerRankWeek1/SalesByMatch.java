736c9888aa58bdc99378ea74ea78cd9017e292e4
oyunmintrio95 <oyunmintrio95@gmail.com>
2024-02-09 14:18:14 -0800

    02092024 HackerRank 1

---
+++ src/HackerRankWeek1/SalesByMatch.java
@@ -0,0 +1,73 @@
+package HackerRankWeek1;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+
+        // Declare a variable for counting pairs.
+        int pair = 0;
+
+        // If n is less than two, pairs cannot exist.
+        if(n <2){
+            return pair;
+        }
+
+        //Use HashSet Data type to find unique value.
+        HashSet<Integer> s = new HashSet<Integer>();
+        for(int i =0; i< ar.size(); i++){
+            //If the one of pair is already in the set, than remove it and add count to 'pair' variable.
+            if(s.contains(ar.get(i))){
+                pair++;
+                s.remove(ar.get(i));
+            }else{
+                // If there's no item in the set, then create new item in the Set.
+                s.add(ar.get(i));
+            }
+        }
+
+        return pair;
+    }
+
+}
+public class SalesByMatch {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
